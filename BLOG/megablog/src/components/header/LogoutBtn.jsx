55a43a0f898626a1fe5a281515d9054c7f00ae4e
Subhas2003 <subhasmunain@gmail.com>
2025-08-29 01:25:48 +0530

    add

---
+++ BLOG/megablog/src/components/header/LogoutBtn.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+// import { AuthService } from '../../appwrite/config'
+import { logout } from '../../store/authSlice'
+import authService from '../../appwrite/auth'
+
+
+const Logout = () => {
+
+    const dispatch = useDispatch()
+    const logoutHandeler = () => {
+        authService.logout().then(() => {
+            dispatch(logout())
+        })
+    }
+
+    return (
+        <button className=' inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'></button>
+    )
+}
+
+export default Logout
--- BLOG/megablog/src/components/index.js
+++ BLOG/megablog/src/components/index.js
@@ -1,7 +1,11 @@
-import Header from './header/header'
-import Footer from './footer/footer'
-
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import Container from "./container/container";
+import Logo from "./footer/logo";
+import LogoutBtn from "./header/LogoutBtn";
 export {
-    Header,
-    Footer
-}
+     Header, 
+     Footer, 
+     Container, 
+     Logo, 
+     LogoutBtn };
